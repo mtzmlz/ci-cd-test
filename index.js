3de424d4d4af53f41db9888493a5f1c95f4bbc04
esauOp <monkeycoss@gmail.com>
2018-03-16 16:04:10 -0600

    .

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 //Load express module with `require` directive
-var express = require('express');
+
 // if(process.env.NODE_ENV !== 'prod') {
 //   require('dotenv').load();
 // }
 
-var app = express();
-const port = process.env.PORT || 8081;
+const server = require('./server');
+const port = process.env.PORT || 8080;
+server.Start(port);
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World! version 4')
-})
-
-//Launch listening server on port 8080
-app.listen(port, function () {
-  console.log(`App listening on port ${port}!`)
-})
+// setTimeout(() => {
+//   server.Stop();
+// }, 2000);
--- oc cheatsheet.sh
+++ oc cheatsheet.sh
@@ -8,10 +8,28 @@ oc new-app https://github.com/mtzmlz/ci-cd-test.git \
     --strategy=source \
     --env PORT=8080 \
     -l name=node-hello
+
+oc new-app https://github.com/mtzmlz/ci-cd-test.git#master \
+    --strategy=source \
+    --env PORT=8080 \
+    -l name=node-hello
 # expose application by creating a service and a route
 oc expose svc/ci-cd-test
+
+# start a new build
+oc start-build ci-cd-test -n ci-cd-demo
 
 # delete application
 oc delete all --all
 # or
 oc delete all -l app=<generated name>
+
+# test github webhook
+curl -H "X-GitHub-Event: push" -H "Content-Type: application/json" \
+    -k -X POST --data-binary \
+    @payload.json \
+    https://api.starter-us-east-1.openshift.com:443/apis/build.openshift.io/v1/namespaces/ci-cd-demo/buildconfigs/ci-cd-test/webhooks/BtZuRFmZOgMmPYupwscg/github
+
+
+# sonarqube token: 74490535ce6acdd4206d85283cc400cda3e98102
+# sonarqube project key: ci-cd-demo2
