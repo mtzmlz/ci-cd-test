3e19a292b858e767cb4b9e3fbbbf9f94f615683c
esauOp <monkeycoss@gmail.com>
2018-03-21 13:11:04 -0600

    .

--- test/test.js
+++ test/test.js
@@ -8,33 +8,33 @@ const port = process.env.PORT || 8080;
 const url = 'http://127.0.0.1:' + port;
 
 describe('Status and content', () => {
-    // before(function() {
-    //     server.Start(port);
-    // });
-    // after(function() {
-    //     server.Stop();
-    // });
-    // it('Should serve root get request', (done) => {
-    //     request(url, (error, response, body) => {
-    //         expect(response.statusCode).to.be.equal(200);
-    //         done();
-    //     });
-    // });
-    // it('Should should greet', (done)=> {
-    //     request(url, (error, response, body) => {
-    //         expect(body).to.equal('Hello World!');
-    //         done();
-    //     });
-    // });
-    // it('Should not serve /about yet', (done) => {
-    //     request(url+'/about', (error, response, body)=>{
-    //         expect(response.statusCode).to.be.equal(404);
-    //         done();
-    //     });
-    // });
-    it('Dummy Test', (done) => {
-        expect(0).to.be.equal(0);
-        done();
+    before(function() {
+        server.Start(port);
     });
+    after(function() {
+        server.Stop();
+    });
+    it('Should serve root get request', (done) => {
+        request(url, (error, response, body) => {
+            expect(response.statusCode).to.be.equal(200);
+            done();
+        });
+    });
+    it('Should should greet', (done)=> {
+        request(url, (error, response, body) => {
+            expect(body).to.equal('Hello World!');
+            done();
+        });
+    });
+    it('Should not serve /about yet', (done) => {
+        request(url+'/about', (error, response, body)=>{
+            expect(response.statusCode).to.be.equal(404);
+            done();
+        });
+    });
+    // it('Dummy Test', (done) => {
+    //     expect(0).to.be.equal(0);
+    //     done();
+    // });
 });
 
